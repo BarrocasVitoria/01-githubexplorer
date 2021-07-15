e10727535689762fb7f9d5abab6ac6facd2e8f07
BarrocasVitoria <vitoriabarrocas2018@gmail.com>
2021-07-14 22:14:38 -0300

    subindo

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 import { render } from 'react-dom'
 import { App } from './App'
 
- //TESTANDO
+render(<App />, document.getElementById('root'))
+// Função do javascrpit para procurar um elemento em tela por um ID: 'getElementById'
+//recebe dois parametros, o primeiro(o que eu quero exibir em tela) o segundo(dentro de qual elemento eu quero exibir essa informação)
